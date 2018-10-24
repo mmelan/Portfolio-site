047dfd905084a3e2e7036de446f5533b78b64e14
Mikael Melan <>
2018-10-24 13:04:26 +0200

    errors corrected

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
-$(document).ready(function(){
-    $(".button").hover(function(){
-        $(this).animate({padding: "15px", fontSize: "2.5em", borderWidth: "3px"});
-        }, function(){
-        $(this).animate({padding: "10px", fontSize: "2em", borderWidth: "2px"});
-    });
-});
+$(document).ready(function () {
+  $('.button').hover(function () {
+    $(this).animate({ padding: '15px', fontSize: '2.5em', borderWidth: '3px' })
+  }, function () {
+    $(this).animate({ padding: '10px', fontSize: '2em', borderWidth: '2px' })
+  })
+})
